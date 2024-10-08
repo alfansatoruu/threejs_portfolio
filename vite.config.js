a9984a252499910d67bcd3fd699ff0bf57d67b60
Jonatandb <jonatandb@gmail.com>
2024-10-08 20:25:34 -0300

    Production build updated

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-})
+  base: mode === "development" ? "/" : "/threejs_portfolio/",
+  build: {
+    outDir: "docs",
+  }
+}))
