0d4e48077af29bab1642009414d34480a99268c7
Jonatandb <jonatandb@gmail.com>
2024-10-12 21:55:16 -0300

    Footer section added

---
+++ src/sections/Footer.jsx
@@ -0,0 +1,41 @@
+const Footer = () => {
+  return (
+    <footer className='c-space pt-7 pb-3 border-t border-black-300 flex justify-between items-center flex-wrap gap-5'>
+      <div className='text-white-500 flex gap-2'>
+        <p>Terms & Conditions</p>
+        <p>|</p>
+        <p>Privacy Policy</p>
+      </div>
+
+      <div className='flex gap-3'>
+        <div className='social-icon'>
+          <img
+            src='assets/github.svg'
+            alt='github icon'
+            className='w-1/2 h-1/2'
+          />
+        </div>
+        <div className='social-icon'>
+          <img
+            src='assets/twitter.svg'
+            alt='twitter icon'
+            className='w-1/2 h-1/2'
+          />
+        </div>
+        <div className='social-icon'>
+          <img
+            src='assets/instagram.svg'
+            alt='instagram icon'
+            className='w-1/2 h-1/2'
+          />
+        </div>
+      </div>
+
+      <p className='text-white-500'>
+        &copy; 2024 - Jonatandb. All Rights Reserved
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
