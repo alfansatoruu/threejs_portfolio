d8ba2ae970fab277124dc6a7ced55fc225c1ff06
Jonatandb <jonatandb@gmail.com>
2024-10-13 15:40:17 -0300

    Missing height fixed

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -41,7 +41,7 @@ const About = () => {
             <img
               src='assets/grid2.png'
               alt='grid-2'
-              className='w-full sm:w-[276px] h-fit object-contain'
+              className='w-full sm:h-[276px] h-fit object-contain'
             />
 
             <div>
