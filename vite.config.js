a4eb699bf3759fad4b699a5857ee3601832fa0db
Jonatandb <jonatandb@gmail.com>
2025-01-22 00:44:09 -0300

    Update Vite config to enable builds to work when uploaded to a subdomain (in this case, /threejs_portfolio/)

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'rewrite-paths',
+      enforce: 'post',
+      generateBundle(options, bundle) {
+        const basePath = mode === "development" ? "/" : "/threejs_portfolio/";
+        Object.values(bundle).forEach((chunk) => {
+          if (chunk.type === 'chunk' || chunk.type === 'asset') {
+            if (chunk.code) {
+              chunk.code = chunk.code
+                .replace(/["']\/assets\//g, `"${basePath}assets/`)
+                .replace(/["']\/models\//g, `"${basePath}models/`)
+                .replace(/["']\/textures\//g, `"${basePath}textures/`);
+            }
+          }
+        });
+      }
+    }
+  ],
   base: mode === "development" ? "/" : "/threejs_portfolio/",
   build: {
     outDir: "docs",
-  }
+  },
 }))
