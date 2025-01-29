74282e837b246c6e8b99554b64bf8259f730dd14
alfansatoruu <hellb2254@gmail.com>
2025-01-29 22:20:50 +0800

    first commit

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className='c-space pt-7 pb-3 border-t border-black-300 flex justify-between items-center flex-wrap gap-5'>
-      <div className='text-white-500 flex gap-2'>
+      <div className='text-black flex gap-2'>
         <p>Terms & Conditions</p>
         <p>|</p>
         <p>Privacy Policy</p>
@@ -17,35 +17,9 @@ const Footer = () => {
             />
           </div>
         </a>
-        {/* <a
-          href='https://twitter.com/jonatandb'
-          target='_blank'
-          rel='noreferrer'
-        >
-          <div className='social-icon'>
-            <img
-              src='assets/twitter.svg'
-              alt='twitter icon'
-              className='w-1/2 h-1/2'
-            />
-          </div>
-        </a> */}
-        {/* <a
-          href='https://www.linkedin.com/in/jonatandb/'
-          target='_blank'
-          rel='noreferrer'
-        >
-          <div className='social-icon'>
-            <img
-              src='assets/linkedin.svg'
-              alt='linkedin icon'
-              className='w-1/2 h-1/2'
-            />
-          </div>
-        </a> */}
       </div>
 
-      <p className='text-white-500'>
+      <p className='text-black'>
         &copy; 2025 - Alvan. All Rights Reserved
       </p>
     </footer>
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -61,10 +61,11 @@ const Hero = () => {
   return (
     <section className='min-h-screen w-full flex flex-col relative' id='home'>
       <div className='w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3'>
-        <p className='sm:text-3xl text-xl font-medium text-white text-center font-generalsans'>
+        <p className='sm:text-3xl text-xl font-medium text-black text-center font-generalsans'>
           Hi, I&apos;m Alvan <span className='waving-hand'>👋🏻</span>
         </p>
       </div>
+
 
       <div className='w-full h-full absolute inset-0'>
         {/* <Leva /> */}
